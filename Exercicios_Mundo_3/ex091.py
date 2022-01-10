7afc29213366edc703211d934f4b6d42a943c3dd
Alex Oliveira <alexoliiveiraa@outlook.com>
2022-01-09 21:26:25 -0300

    Exercícos de dicionários

--- Exercicios_Mundo_3/ex091.py
+++ Exercicios_Mundo_3/ex091.py
@@ -4,3 +4,19 @@ No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maio
 
 from random import randint
 from time import sleep
+from operator import itemgetter
+
+print('-=' * 30)
+jogos = {'Jogador_1': randint(1, 6),
+         'Jogador_2': randint(1, 6),
+         'Jogador_3': randint(1, 6),
+         'Jogador_4': randint(1, 6),
+         }
+for jogador, jogo in jogos.items():
+    print(f'O {jogador} tirou {jogo} no dado.')
+    sleep(0.5)
+print('-=' * 30)
+ranking = sorted(jogos.items(), key=itemgetter(1), reverse=True)
+for jogador, jogo in enumerate(ranking):
+    print(f'O {jogo[0]} ficou em {jogador + 1}º lugar com {jogo[1]} ')
+    sleep(0.5)
